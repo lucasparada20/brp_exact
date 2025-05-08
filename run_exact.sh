e12f167c14eeb7cff2e9a57c6a4174940d0c1180
Lucas Parada <lucasparada20@gmail.com>
2025-05-08 17:34:10 -0400

    Add files via upload

---
+++ run_exact.sh
@@ -0,0 +1,7 @@
+#build/exec_exact instance_type=dhin instance_file=instances_dhin/Bari20.txt
+
+#build/exec_exact instance_type=dhin instance_file=instances_dhin/CiudadDeMexico17.txt  
+
+#build/exec_exact instance_type=slr instance_file=instances_slr/boston424.txt targets_file_name=instances_slr/targets/targets_boston.txt initial_capacities_file_name=instances_slr/status/boston_station_status.json delta=1
+
+#build/exec_exact instance_type=slr instance_file=instances_slr/sanfrancisco555.txt targets_file_name=instances_slr/targets/targets_sanfrancisco.txt initial_capacities_file_name=instances_slr/status/sanfrancisco_station_status.json delta=1
